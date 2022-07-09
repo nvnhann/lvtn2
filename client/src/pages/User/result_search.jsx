5efc7b0c8f916f567ab758cfa934d9199ed437f4
Khoa Đào Minh <mihkhoa.dev@gmail.com>
2022-07-09 18:13:48 +0700

    up

--- client/src/pages/User/result_search.jsx
+++ client/src/pages/User/result_search.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Tab from '@mui/material/Tab'
-import TabContext from '@mui/lab/TabContext'
-import TabList from '@mui/lab/TabList'
-import TabPanel from '@mui/lab/TabPanel'
-import {IoMdFolderOpen} from 'react-icons/io'
+import React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { IoMdFolderOpen } from "react-icons/io";
 
 function ResultSearch() {
-  const [value, setValue] = React.useState('1')
+  const [value, setValue] = React.useState("1");
 
   const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+    setValue(newValue);
+  };
   return (
     <div className="w-[80%] mx-auto">
-      <Box sx={{width: '100%', magrin: 'center', typography: 'body1'}}>
+      <Box sx={{ width: "100%", magrin: "center", typography: "body1" }}>
         <TabContext value={value}>
-          <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
               <Tab label="Tất cả" value="1" />
               <Tab label="Khóa học" value="2" />
               <Tab label="Giáo viên" value="3" />
-              <Tab label="Bài báo" value="4" />
+              <Tab label="Sinh viên" value="4" />
+              <Tab label="Bài báo" value="5" />
             </TabList>
           </Box>
           <TabPanel value="1">
@@ -34,7 +35,10 @@ function ResultSearch() {
                 </div>
               </div>
               <div className="h-[150px]">
-                <IoMdFolderOpen size={25} className="absolute bottom-2 right-2 cursor-pointer" />
+                <IoMdFolderOpen
+                  size={25}
+                  className="absolute bottom-2 right-2 cursor-pointer"
+                />
               </div>
             </div>
           </TabPanel>
@@ -49,7 +53,10 @@ function ResultSearch() {
                 </div>
               </div>
               <div className="h-[150px]">
-                <IoMdFolderOpen size={25} className="absolute bottom-2 right-2 cursor-pointer" />
+                <IoMdFolderOpen
+                  size={25}
+                  className="absolute bottom-2 right-2 cursor-pointer"
+                />
               </div>
             </div>
           </TabPanel>
@@ -76,10 +83,33 @@ function ResultSearch() {
           </TabPanel>
 
           <TabPanel value="4">
+            <div className="flex p-4 gap-4 items-center rounded-md bg-white">
+              <div className="w-[80px] h-[80px] bg-slate-300 rounded-full"></div>
+              <div>
+                <p className="text-[20px] font-bold">Đào Minh Pha</p>
+                <p>
+                  <strong>MSSV: </strong>B1809248
+                </p>
+                <p>
+                  <strong>Chuyên nghành : </strong>Công nghệ thông tin
+                </p>
+              </div>
+              <div className="ml-auto mr-0">
+                <button className="py-2 px-8 font-bold bg-[#A7DDFB] text-[#2554A6] rounded-md">
+                  Xem
+                </button>
+              </div>
+            </div>
+            <p className="mt-4 text-center font-bold">Xem thêm</p>
+          </TabPanel>
+
+          <TabPanel value="5">
             <div className="flex p-4 gap-5 bg-white rounded-md">
               <div className="w-[30%] h-[200px] bg-slate-400 rounded-md"></div>
               <div className="w-[70%]">
-                <p className="text-[25px] font-bold">Tạp chí khoa học công nghệ thế giới</p>
+                <p className="text-[25px] font-bold">
+                  Tạp chí khoa học công nghệ thế giới
+                </p>
                 <p>Tạp chí của Nguyễn Văn A</p>
                 <p className="text-[14px]">
                   <i>Ngày 30/6/2022</i>
@@ -99,7 +129,7 @@ function ResultSearch() {
         </TabContext>
       </Box>
     </div>
-  )
+  );
 }
 
-export default ResultSearch
+export default ResultSearch;
